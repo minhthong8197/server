de96416cd375a963073526bb20cf6928bf70fa18
minhthong8197 <minhthong8197@gmail.com>
2019-01-03 05:49:03 +0700

    Initial commit

---
+++ app/authentication.js
@@ -0,0 +1,146 @@
+
+
+var MongoClient = require('mongodb').MongoClient;
+var config = require('../_config');
+var database = require('../app/database');
+var Q = require('q');
+const bcrypt = require('bcrypt');
+const saltRounds = 3;
+
+exports = module.exports = {};
+
+/**
+ * Hash a normal password to special string for save to database
+ * @param {string} password 
+ */
+exports.hashPassword = async (password) => {
+    var deferred = Q.defer();
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        if (err) {
+            deferred.reject(new Error(err));
+        } else {
+            deferred.resolve(hash);
+        }
+    });
+    
+    return deferred.promise;
+}
+
+/**
+ * Compare input password match with a hash password
+ * @name comparePassword
+ * @author phieu-th
+ */
+exports.comparePassword = async (password, hashPassword) => {
+    var deferred = Q.defer();
+
+    bcrypt.compare(password, hashPassword, (err, result) => {
+        if (err) {
+            deferred.reject(new Error(err));
+        } else {
+            deferred.resolve(result);
+        }
+    })
+
+    return deferred.promise;
+}
+
+/**
+ * Check an username existed in DB collection Users 
+ * @author phieu-th
+ * @async
+ * @param {string} username 
+ */
+exports.isExistUsername = async (username) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            collection = db.collection(database.iTravelDB.Users, (err, userCollection) => {
+                var find = userCollection.findOne({ username: { $eq: username } }, (err, result) => {
+                    if (err) {
+                        deferred.resolve(false);
+                    } else {
+                        if (result) {
+                            deferred.resolve(true);
+                        }
+                        deferred.resolve(false);
+                    }
+                });
+            })
+        }
+        
+        client.close();
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Save log user Sign In to system. This funtion don't need to return
+ * @name insertUserSignInLog
+ * @author phieu-th
+ * @param {string} username 
+ */
+exports.insertUserSignInLog = async (username) => {
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            collection = db.collection(database.iTravelDB.SignInHistory, (err, signInHistoryCollection) => {
+                var dateTimeSignIn = new Date()
+                var signInLog = {
+                    username: username,
+                    creationTime: dateTimeSignIn
+                }
+
+                signInHistoryCollection.insertOne(signInLog);
+            })
+        }
+        
+        client.close();
+    });
+};
+
+exports.isAdminUser = async (username) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            collection = db.collection(database.iTravelDB.Users, (err, userCollection) => {
+                var find = userCollection.findOne({ username: { $eq: username } }, (err, result) => {
+                    if (err) {
+                        deferred.resolve(false);
+                    } else {
+                        if (result && result.permission === config.USER_PERMISSION.Admin) {
+                            deferred.resolve(true);
+                        }
+                        deferred.resolve(false);
+                    }
+                });
+            })
+        }
+        
+        client.close();
+    });
+
+    return deferred.promise;
+}
---
+++ app/database.js
@@ -0,0 +1,444 @@
+
+var MongoClient = require('mongodb').MongoClient;
+var config = require('../_config');
+var Q = require('q');
+
+exports = module.exports = {};
+
+const iTravelDB = {
+    Provinces: 'Provinces',
+    Menu: 'Menu',
+    Feedback: 'Feedback',
+    Posts: 'Posts',
+    SearchHistory: 'SearchHistory',
+    Tags: 'Tags',
+    PostCategories: 'PostCategories',
+    ProvinceCity: 'ProvinceCity',
+    Locations: 'Locations',
+    Users: 'Users',
+    SignInHistory: 'SignInHistory',
+    Policies: 'Policies'
+}
+
+exports.iTravelDB = iTravelDB;
+
+/**
+ * Get a collection by it's name
+ * @author phieu-th
+ * @async
+ * @param {string} collectionName 
+ */
+exports.getCollection = async function (collectionName) {
+    var deferred = Q.defer();
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName);
+
+            deferred.resolve(collection);
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Get all documents in a collection in db
+ * @author phieu-th
+ * @async
+ * @param {string} collectionName 
+ */
+exports.getCollectionData = async (collectionName) => {
+    var deferred = Q.defer();
+    var data = null;
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    deferred.reject(new Error(err));
+                }
+
+                var collectionData = collection.find().toArray((err, result) => {
+                    if (err) {
+                        console.log('Error find data from collection ' + collectionName);
+                        deferred.reject(new Error(err));
+                    } else {
+                        data = result;
+                        deferred.resolve(data);
+                    }
+                });
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Get data in a collection have filter
+ * @param {string} collectionName 
+ * @param {object} filter 
+ */
+exports.getCollectionFilterData = async (collectionName, filter) => {
+    var deferred = Q.defer();
+    var data = null;
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    deferred.reject(new Error(err));
+                }
+
+                var collectionData = collection.find(filter).toArray((err, result) => {
+                    if (err) {
+                        console.log('Error find filter data from collection ' + collectionName);
+                        deferred.reject(new Error(err));
+                    } else {
+                        data = result;
+                        deferred.resolve(data);
+                    }
+                });
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Insert a document to a colection by colection name
+ * @author phieu-th
+ * @async
+ * @param {string} collectionName 
+ * @param {object} document 
+ */
+exports.insertOneToColection = async (collectionName, document) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    deferred.reject(new Error(err));
+                }
+
+                try {
+                    var insertOne = collection.insertOne(document, (err) => {
+                        if (err) {
+                            console.log('Error insert data to ' + collectionName);
+                            deferred.reject(new Error(err));
+                        }
+                        return 1;
+                    });
+                    deferred.reject(insertOne);
+                }
+                catch (e) {
+                    console.log('Error insert data to ' + collectionName);
+                    console.log(e.message);
+                    deferred.reject(new Error(e));
+                }
+            });
+
+            client.close();
+        }
+    });
+}
+
+/**
+ * Get only one document match with filter
+ * @name getOneFromCollection
+ * @author phieu-th
+ * @async
+ * @param {string} collectionName 
+ * @param {object} filter 
+ */
+exports.getOneFromCollection = async (collectionName, filter) => {
+    var deferred = Q.defer();
+    var data = null;
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    return null;
+                }
+
+                var collectionData = collection.findOne(filter, (err, result) => {
+                    if (err) {
+                        console.log('Error find filter data from collection ' + collectionName);
+                        return null;
+                    } else {
+                        data = result;
+                        deferred.resolve(data);
+                    }
+                });
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Update only one document be chosen by filter with new properties
+ * @name updateDocumentByFilter
+ * @async
+ * @author phieu-th
+ * @param {string} collectionName 
+ * @param {object} documentFiler 
+ * @param {object} changeProperties 
+ */
+exports.updateDocumentByFilter = async (collectionName, documentFiler, changeProperties) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    return deferred.reject(new Error(err));;
+                }
+
+                try {
+                    collection.updateOne(documentFiler, { $set: changeProperties })
+                        .then((result) => {
+                            deferred.resolve(result);
+                        })
+                }
+                catch (e) {
+                    console.log('Error update ' + documentFiler + 'data to ' + collectionName);
+                    console.log(e.message);
+                    deferred.reject(new Error(e));
+                }
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * @name replayDocumentById
+ * @author Thong
+ * @description replay a document by new one, keep the same id
+ * @param {string} collectionName
+ * @param {object} documentFiler the object filter use to find the old document need replay
+ * @param {Post} changeDocument new document use to replay the old one
+ */
+exports.replayDocumentById = async (collectionName, documentFiler, changeDocument) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    return deferred.reject(new Error(err));;
+                }
+
+                try {
+                    collection.replaceOne(documentFiler, changeDocument)
+                        .then((result) => {
+                            deferred.resolve(result);
+                        })
+                }
+                catch (e) {
+                    console.log('Error replay ' + 'data to ' + collectionName);
+                    console.log(e.message);
+                    deferred.reject(new Error(e));
+                }
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * @name getOneWithProjection
+ * @author Thong
+ * @description get one document of a collection, user filter to find document, 
+ * use projection to filtout collumns do not need
+ * @param {string} collectionName
+ * @param {object} filter the object filter use to find the true document
+ * @param {object} projectionObj use to choose collumns to return
+ */
+exports.getOneWithProjection = async (collectionName, filter, projectionObj) => {
+    var deferred = Q.defer();
+    var data = null;
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    return null;
+                }
+
+                var collectionData = collection.findOne(filter, projectionObj, (err, result) => {
+                    if (err) {
+                        console.log('Error find filter data from collection ' + collectionName);
+                        return null;
+                    } else {
+                        data = result;
+                        deferred.resolve(data);
+                    }
+                });
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * Get number of document match with filter
+ * @name countDocumentByFilter
+ * @author phieu-th
+ * @param {sting} collectionName 
+ * @param {object} filter 
+ */
+exports.countDocumentByFilter = async (collectionName, filter) => {
+    var deferred = Q.defer();
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    return null;
+                }
+
+                if (filter !== undefined && filter !== null) {
+                    collection.countDocuments(filter, (err, result) => {
+                        if (err) {
+                            console.log('Error find count document from collection ' + collectionName);
+                            deferred.reject(new Error(err));
+                        } else {
+                            deferred.resolve(result);
+                        }
+                    });
+                } else {
+                    collection.countDocuments((err, result) => {
+                        if (err) {
+                            console.log('Error find count document from collection ' + collectionName);
+                            deferred.reject(new Error(err));
+                        } else {
+                            deferred.resolve(result);
+                        }
+                    });
+                }
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
+
+exports.getProjectCollectionDataByFilter = async (collectionName, filter, project) => {
+    var deferred = Q.defer();
+    var data = null;
+
+    MongoClient.connect(config.CONNECTION_STRING, { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            console.log("Get Connection has an error: " + err.message);
+            deferred.reject(new Error(err));
+        } else {
+
+            var db = client.db(config.DB_NAME);
+
+            var collection = db.collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error load ' + collectionName);
+                    deferred.reject(new Error(err));
+                }
+
+                var collectionData = collection.find(filter).project(project).toArray((err, result) => {
+                    if (err) {
+                        console.log('Error find filter data from collection ' + collectionName);
+                        deferred.reject(new Error(err));
+                    } else {
+                        data = result;
+                        deferred.resolve(data);
+                    }
+                });
+            });
+
+            client.close();
+        }
+    });
+
+    return deferred.promise;
+}
